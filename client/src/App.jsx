44ee4571cafeeadd9083fc2169c6b666ddf162ae
Sonupr130 <sonupradhan130@gmail.com>
2025-03-06 23:26:47 +0530

    routing setup for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 const App = () => {
   return (
-    <div className='h-scren w-screen bg-gray-100 flex justify-center items-center'>
-      <h1 className='text-4xl text-blue-500'>Hello World</h1>
-      <Button>Click Me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
